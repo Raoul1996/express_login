a436f03c8e0ea961bd9215f1e8f8dd981f30a86b
Raoul1996 <baobaozhou199606@gmail.com>
2017-07-16 05:39:24 +0000

    register users successfully

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,33 @@
  */
 let express = require('express')
 let router = express.Router()
-router.get('/', function (req, res, next) {
+let mongoose = require('mongoose')
+router.route('/').get(function (req, res, next) {
   res.render('login')
 })
+  .post(function (req, res, next) {
+    let username = req.body.username
+    let password = req.body.password
+    if (!username || !password) {
+      res.send('<p>your should submit the username and password</p>')
+      return
+    }
+    let db = mongoose.createConnection('mongodb://123.207.252.230:12345/test')
+    let User = db.model('user', new mongoose.Schema({
+      username: String,
+      password: String
+    }))
+    User.findOne({username: username}, 'password', function (err, data, affectNums) {
+      if (!data) {
+        res.send('<p>this username is not exists!</p>')
+        return
+      }
+      if (data.password === password) {
+        req.session.user = {username: username}
+        res.redirect('/')
+        return
+      }
+      res.send('<p>your password is error!</p>')
+    })
+  })
 module.exports = router
--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,57 @@
  */
 let express = require('express')
 let router = express.Router()
-router.get('/', function (req, res, next) {
-  res.render('register')
-})
+let mongoose  = require('mongoose')
+router.route('/')
+  .get(function (req, res, next) {
+    res.render('register')
+  })
+  .post(function (req, res, next) {
+    let username = req.body.username
+    let password = req.body.password
+    let passwordConfirm = req.body['password-confirm']
+    let userData = {username: username, password: password}
+    if (!username || !password) {
+      res.json({
+        'message': 'Password or userName is empty!'
+      })
+      return
+    }
+    if (password !== passwordConfirm) {
+      res.json({
+        'message': 'Password and confirm password do not match!'
+      })
+      return
+    }
+    let db = mongoose.createConnection('mongodb://123.207.252.230:12345/test')
+    let Schema = new mongoose.Schema({
+      username: String,
+      password: String
+    })
+    let User = db.model('user', Schema)
+    User.findOne({username: username}, function (err, data) {
+      if (err) {
+        res.send(err)
+        return
+      }
+      if (data) {
+        res.send('<p>Account already exists</p>')
+        return
+      }
+      User.create(userData, function (err, data, afterNums) {
+        if (err) {
+          res.json({
+            message: err
+          })
+          return
+        }
+        if (afterNums === 0) {
+          res.send('<p>Register Error</p>')
+          return
+        }
+        req.session.user = {username: username}
+        res.redirect('/')
+      })
+    })
+  })
 module.exports = router
