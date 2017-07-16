332060ff97a6e8859725492b897581b5af6ab1cc
Raoul1996 <baobaozhou199606@gmail.com>
2017-07-16 04:37:05 +0000

    layout the pages completed

---
+++ routes/login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by raoul on 17-7-16.
+ */
+let express = require('express')
+let router = express.Router()
+router.get('/', function (req, res, next) {
+  res.render('login')
+})
+module.exports = router
